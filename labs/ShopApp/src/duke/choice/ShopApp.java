8e6036f889a3cc0a6d43edd059736cd5679c8f70
Sara Herrington <sara.herrington@outlook.com>
2020-10-24 21:13:13 -0500

    Operators lab

--- labs/ShopApp/src/duke/choice/ShopApp.java
+++ labs/ShopApp/src/duke/choice/ShopApp.java
@@ -1,12 +1,31 @@
 package duke.choice;
 
 public class ShopApp {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Duke Choice Shop!");
+  public static void main(String[] args) throws Exception {
+    double tax = 0.2;
+    
+    System.out.println("Welcome to Duke Choice Shop!");
+    
+    Customer c1 = new Customer();
+    c1.name = "Pinky";
+    System.out.println("Customer is " + c1.name + "!");
 
-        Customer c1 = new Customer();
-        c1.name = "Pinky";
+    Clothing item1 = new Clothing();
+    item1.description = "Blue jacket";
+    item1.price = 20.9;
+    item1.size = "M";
 
-        System.out.println("Customer is " + c1.name + "!");
-    }
+    Clothing item2 = new Clothing();
+    item2.description = "Orange T-shirt";
+    item2.price = 10.5;
+    item2.size = "S";
+
+    System.out.println("Item 1: " + item1.description + ", " + item1.size + ", $" + item1.price);
+    System.out.println("Item 2: " + item2.description + ", " + item2.size + ", $" + item2.price);
+    System.out.println("Item 3: " + item2.description + ", " + item2.size + ", $" + item2.price);
+
+    double subTotal = item1.price + item2.price * 2;
+    double total = subTotal + (subTotal * tax);
+    System.out.println("Total: $" + total);
+  }
 }
